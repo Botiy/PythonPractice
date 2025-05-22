1887152fc561dbdeeb959e9899522e4c2e9c6245
Botiy <horvath.botond425@gmail.com>
2025-05-22 11:49:25 +0200

    Update_sum_counter

--- PythonAdvanced.py
+++ PythonAdvanced.py
@@ -35,3 +35,13 @@ def iteration(name, age, height):
 
 iteration('Boti', 23, 195)
 
+def sum_counter():
+    nl = [0,1,2,3,4,5,6,7,8,9,10]
+
+    count = 0
+    for item in nl:
+        count += item
+        print(count)
+
+sum_counter()
+
